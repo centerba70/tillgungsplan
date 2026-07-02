f5db5fa67bfe8327c5b7f09ff330ea67737e6da9
Francesco Buccini <francesco.buccini1@gmail.com>
2026-07-02 13:07:41 +0200

    First Commit from Spring Initializr

---
+++ src/main/java/com/coding_challenge/tillgungsplan/TillgungsplanApplication.java
@@ -0,0 +1,13 @@
+package com.coding_challenge.tillgungsplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TillgungsplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TillgungsplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding_challenge/tillgungsplan/TillgungsplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding_challenge.tillgungsplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TillgungsplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
